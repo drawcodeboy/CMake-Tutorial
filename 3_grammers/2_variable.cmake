da2a1e91667ab0b4c64828cf9efc5df87b5681df
drawcodeboy <20rnjsekdns@naver.com>
2025-01-22 18:03:26 +0900

    [Add] variables categories

--- 3_grammers/2_variable.cmake
+++ 3_grammers/2_variable.cmake
@@ -7,3 +7,20 @@ message("test message")
 set(A value) # DON'T set(A, value)! NO COMMA!
 message(${A}) # Access Variable ${}
 
+# Remove Variable, unset(<variable> [CACHE | PARENT_SCOPE])
+unset(A)
+
+# Variable got three categories.
+# Function, Directory, Persistent Cache.
+
+# 1. Function Category
+# It's valid in range of function()/endfunction()
+set(VAR value)
+
+function(foo)
+    set(VAR modified_value)
+endfunction()
+
+foo()
+message(${VAR}) # It will print value, because VAR is Function Category
+
